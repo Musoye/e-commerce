b78a6fee5a86566f8e74f3fa58529b13e1b9e730
Musoye <oyebamijimustapha44@gmail.com>
2022-11-09 05:10:46 -1100

    new features like increase button oin each element;

--- commerce.js
+++ commerce.js
@@ -48,17 +48,50 @@ function DisplayElem(elementid)
     let element = document.getElementById(elementid);
     element.style.display = 'block';
 }
+// Not display Element
 function NotDisplayElem(elementid)
 {
     let element = document.getElementById(elementid);
     element.style.display = 'none';
 }
 
+
+//Increse each body element
+function IncreaseNum(elementid)
+{
+    let element = document.getElementById(elementid);
+    let coun = parseInt(element.innerHTML);
+    coun++;
+    element.innerHTML = coun;
+}
+// Decrease each body element
+function DecreaseNum(elementname)
+{
+    let last = elementname.charAt(elementname.length - 1);
+    console.log(last);
+    let element = document.getElementById(elementname);
+    let coun = parseInt(element.innerHTML);
+    if (coun >= 2)
+    {
+        coun--;
+        element.innerHTML = coun;
+    }
+    else
+    {
+        alert(`The quantity can't be less than 1 and it will be removed,So if you still eant to buy it add the item back in the aAdd to Cart Menu. Thank you!:`);
+        let elel = 'pixel' + last;
+        let p = 'p' + last;
+        // console.log(p);
+        // console.log(elel);
+        RemoveFromCart(p, elel);
+    }
+}
 //Displaying pop-up page
 function displayPopPay() {
     let pop = document.getElementById('pay');
     pop.style.display = 'block';
 }
+
 //Nullifying the pop and reload the entire webpage
 function ReloadSession() {
     location.reload();
@@ -73,17 +106,26 @@ function RemovePopPay() {
 function RemoveFromCart(elementid, elementname) {
     let Para = document.getElementById(elementid);
     let counter = document.getElementById('spa');
+    let lastem = elementid.charAt(elementid.length - 1);
+    // console.log(elementid);
+    // console.log(lastem);
+    let newelem = 'present' + lastem;
+    console.log(newelem);
     // console.log(elementname);
 
     if (Para.innerHTML === 'ADD TO CART') {
         DisplayElem(elementname);
         count = count + 1;
         counter.innerHTML = count;
-        Para.innerHTML = "REMOVE FROM THE CART"
+        Para.innerHTML = "REMOVE FROM THE CART";
         Para.style.backgroundColor = " #FF7A00";
+        let newelement = document.getElementById(newelem);
+        newelement.innerHTML = 1;
     }
     else {
         NotDisplayElem(elementname);
+        //let newelement = document.getElementById(newelem);
+        //newelement.innerHTML = 0;
         if (count > 0) {
             count = count - 1;
             counter.innerHTML = count;
@@ -97,7 +139,8 @@ function RemoveFromCart(elementid, elementname) {
         }
     }
 }
-//let q = RemoveFromCart('p1', 'nomen1');
+
+//calling each element with its dom
 let button1 = document.getElementById('p1');
 let button2 = document.getElementById('p2');
 let button3 = document.getElementById('p3');
@@ -121,4 +164,55 @@ button5.onclick = function(){
 }
 button6.onclick = function(){
     RemoveFromCart('p6', 'pixel6');  
+}
+
+//calling each increment with its dom
+let btn1 = document.getElementById('btn-sale1');
+let btn2 = document.getElementById('btn-sale2');
+let btn3 = document.getElementById('btn-sale3');
+let btn4 = document.getElementById('btn-sale4');
+let btn5 = document.getElementById('btn-sale5');
+let btn6 = document.getElementById('btn-sale6');
+btn1.onclick = function(){
+    IncreaseNum('present1');
+}
+btn2.onclick = function(){
+    IncreaseNum('present2');
+}
+btn3.onclick = function(){
+    IncreaseNum('present3');
+}
+btn4.onclick = function(){
+    IncreaseNum('present4');
+}
+btn5.onclick = function(){
+    IncreaseNum('present5');
+}
+btn6.onclick = function(){
+    IncreaseNum('present6');
+}
+//calling each increment with its dom
+let nbtn1 = document.getElementById('btn-sal1');
+let nbtn2 = document.getElementById('btn-sal2');
+let nbtn3 = document.getElementById('btn-sal3');
+let nbtn4 = document.getElementById('btn-sal4');
+let nbtn5 = document.getElementById('btn-sal5');
+let nbtn6 = document.getElementById('btn-sal6');
+nbtn1.onclick = function(){
+    DecreaseNum('present1') ;
+}
+nbtn2.onclick = function(){
+    DecreaseNum('present2') ;
+}
+nbtn3.onclick = function(){
+    DecreaseNum('present3') ;
+}
+nbtn4.onclick = function(){
+    DecreaseNum('present4') ;
+}
+nbtn5.onclick = function(){
+    DecreaseNum('present5') ;
+}
+nbtn6.onclick = function(){
+    DecreaseNum('present6') ;
 }
